b714a8f54ed6af312d838489333c774f74a1b9e3
enckse <enck@development>
2012-08-24 22:48:46 -0400

    Adding basic assembly info definitions

---
+++ StyleCopCmd.Console/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+//------------------------------------------------------------------------------
+// <copyright 
+//  file="AssemblyInfo.cs" 
+//  company="enckse">
+//  Copyright (c) All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("StyleCopCmd.Console")]
+
+[assembly: AssemblyDescription("")]
+
+[assembly: AssemblyConfiguration("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+
+[System.CLSCompliant(true)]
+
+[System.Runtime.InteropServices.ComVisible(false)]
