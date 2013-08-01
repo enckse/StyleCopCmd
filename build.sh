533f9b50ed48adf756480d7f8f43e8fa42d8fbc2
Sean Enck <enckse@gmail.com>
2013-07-31 20:21:40 -0400

    Updating the simple build script to check for required files

--- build.sh
+++ build.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+if ! hash xbuild 2>/dev/null; then
+	echo "xbuild is required to use the build script"	
+	exit 1
+fi
+
+if ! hash nunit-console2 2>/dev/null; then
+	echo "nunit (nunit-console2) is required to use the build script"	
+	exit 1
+fi
+
+if ! hash mono 2>/dev/null; then
+	echo "mono is required to use the build script"	
+	exit 1
+fi
 
 # Build type
 BUILD="$(tr [A-Z] [a-z] <<< "$1")"
