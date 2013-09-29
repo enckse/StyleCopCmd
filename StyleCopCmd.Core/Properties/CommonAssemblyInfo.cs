d9570c99ac509364761f193ae48d83f82450e6b0
Sean Enck <enckse@gmail.com>
2013-09-29 09:22:44 -0400

    Added the common file propertie

---
+++ StyleCopCmd.Core/Properties/CommonAssemblyInfo.cs
@@ -0,0 +1,19 @@
+//------------------------------------------------------------------------------
+// <copyright 
+//  file="CommonAssemblyInfo.cs" 
+//  company="enckse">
+//  Copyright (c) All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyDescription("")]
+
+[assembly: AssemblyConfiguration("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+
+[assembly: System.CLSCompliant(true)]
+
+[assembly: System.Runtime.InteropServices.ComVisible(false)]
