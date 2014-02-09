a15f573015c94c4574d47f78d98e3094a7fbf52b
Sean Enck <enckse@gmail.com>
2014-02-08 21:24:54 -0500

    Added missing stylecop mock implementation for testing

---
+++ StyleCopCmd.Core.Test/StyleCopRunnerMock.cs
@@ -0,0 +1,18 @@
+//------------------------------------------------------------------------------
+// <copyright 
+//  file="StyleCopRunnerMock.cs" 
+//  company="enckse">
+//  Copyright (c) All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+namespace StyleCopCmd.Core
+{
+    using StyleCop;
+
+    /// <summary>
+    /// Testing runner
+    /// </summary>
+    public class StyleCopRunnerMock : StyleCopRunner
+    {
+    }
+}
